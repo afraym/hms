0d80cad91ac17370d1cb1fb98d2a3db6a20c1342
afraym <afraymn@gmail.com>
2025-06-26 13:20:12 +0300

    fix: update default string length to 191 in AppServiceProvider for database compatibility

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(191);
 
     }
 }
