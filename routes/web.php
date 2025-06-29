a18d07494a4bfeddee6fcbbaf52d163d121ad2da
afraym <afraymn@gmail.com>
2025-06-29 12:13:08 +0300

    fix: comment out unused dashboard route for admin and manager roles

--- routes/web.php
+++ routes/web.php
@@ -48,9 +48,9 @@ Route::view('profile', 'profile')
         Route::get('patients/export', [PatientController::class, 'export'])->name('patients.export');
     });
 
-    Route::middleware(['auth', 'checkrole:admin|manager'])->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-    });
+    // Route::middleware(['auth', 'checkrole:admin|manager'])->group(function () {
+    //     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    // });
 
 // require __DIR__.'/auth.php';
 
