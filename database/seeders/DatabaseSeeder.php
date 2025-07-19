69a2df41016f7368c08f776f8e8103ae5a7fb98a
afraym <afraym@users.noreply.github.com>
2025-07-20 01:44:38 +0300

    fix: Correct seeder calls by enabling DepartmentSeeder and disabling PatientsTableSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(DepartmentSeeder::class);
-        $this->call(PatientsTableSeeder::class);
+        $this->call(DepartmentSeeder::class);
+        // $this->call(PatientsTableSeeder::class);
         
 
         // User::factory(10)->create();
